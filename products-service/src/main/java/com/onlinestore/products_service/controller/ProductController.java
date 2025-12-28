ac42bd6ce796dc605a68bc5563128cb29d16e81f
matias <rmatias.dev@gmail.com>
2025-12-28 17:22:51 -0300

    feat(controller): add endpoints for basic CRUD operations

--- products-service/src/main/java/com/onlinestore/products_service/controller/ProductController.java
+++ products-service/src/main/java/com/onlinestore/products_service/controller/ProductController.java
@@ -1,4 +1,42 @@
 package com.onlinestore.products_service.controller;
 
-public class ProductController {
+import com.onlinestore.products_service.dto.ProductDTO;
+import com.onlinestore.products_service.model.Product;
+import com.onlinestore.products_service.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController{
+
+    @Autowired
+    private IProductService iProductService;
+
+    @PostMapping("/create")
+    public String createProduct(@RequestBody Product product){
+        return iProductService.createProduct(product);
+    }
+
+    @PutMapping("/update")
+    public String updateProduct(@RequestBody Product product){
+        return iProductService.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        return iProductService.deleteProduct(id);
+    }
+
+    @GetMapping("/find/{id}")
+    public ProductDTO findByProductId(Long id){
+        return iProductService.findByProductId(id);
+    }
+
+    @GetMapping("/find-all")
+    public List<ProductDTO> findAllProducts(){
+        return iProductService.findAllProducts();
+    }
 }
